9b16eb4f59c049ec81579bb3f6e3ba0d1a56098c
Danny Tomkins <tomkinsdan@gmail.com>
2021-02-27 14:00:49 -0600

    first commit

---
+++ client/src/TodoApp.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function TodoApp() {
+  return (
+    <div className="container">
+      <Header message="Todo List" />
+      <TodoForm />
+      <TodoList />
+      hello world
+    </div>
+  );
+}
+
+export default App;
